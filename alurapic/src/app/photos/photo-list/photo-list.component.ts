bc81be0236e3c237a261b2af07dbf45f742ea718
Rodrigo Cavalcanti <rodrigo.cavalcanti-silva@gmail.com>
2020-05-29 14:55:42 -0300

    Paginação

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,27 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/Photo';
-
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+import { PhotoService } from '../photo/photo.service';
+ 
 @Component({
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
-  constructor(
-      private photoService: PhotoService,
-      private activateRoute: ActivatedRoute
-  ) {
-
-  }
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
+  filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentePage: number = 1;
+  userName: string = '';
+
+  constructor(
+      private activetedRoute: ActivatedRoute,
+      private photoService: PhotoService
+  ) { }
 
   ngOnInit(): void {
-    const username = this.activateRoute.snapshot.params.username;
-    this.photoService
-    .listFromUser(username)
-    .subscribe(photos => this.photos = photos);
+    this.userName = this.activetedRoute.snapshot.params.userName;
+    this.photos = this.activetedRoute.snapshot.data['photos'];
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe( filter => this.filter  = filter );
   }
 
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+  load(){
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentePage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+      if(!photos.length) this.hasMore = false;
+      });
+  }
 }
--- alurapic/src/app/photos/photo/photo.service.ts
+++ alurapic/src/app/photos/photo/photo.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Photo } from "./Photo";
+import { PARAMETERS } from '@angular/core/src/util/decorators';
 
 const API = 'http://localhost:3000';
 
@@ -11,5 +12,10 @@ export class PhotoService {
     listFromUser(userName: string) {
         return this.http
             .get<Photo[]>(API + '/' + userName + '/photos');
+    }
+    listFromUserPaginated(userName: string, page: number) {
+        const params = new HttpParams().append('page', page.toString());
+        return this.http
+            .get<Photo[]>(API + '/' + userName + '/photos', { params });
     }
 }
