4dd5eeaac4a2a6ba16095f5076516b96492a293a
Rodrigo Cavalcanti <rodrigo.cavalcanti-silva@gmail.com>
2020-06-01 13:26:48 -0300

    Criação do componente de mensagem

---
+++ alurapic/src/app/shared/components/vmessage/vmessage.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { VMessagemComponent } from './vmessage.component';
+
+@NgModule({
+    declarations: [ VMessagemComponent ],
+    exports: [ VMessagemComponent ]
+})
+export class VMessageModule{
+
+
+}
