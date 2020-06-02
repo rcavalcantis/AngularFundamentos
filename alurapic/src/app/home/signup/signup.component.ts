3ee6122e4d06a49c42fe7021382196e9f31896f3
Rodrigo Cavalcanti <rodrigo.cavalcanti-silva@gmail.com>
2020-06-02 15:56:34 -0300

    SignUP de novos usuários

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { NewUser } from './new-user';
+import { SignUpService } from './signup.service';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -12,7 +16,9 @@ export class SignUpComponent implements OnInit {
 
     constructor(
             private formBuilder: FormBuilder,
-            private userNotTakenValidatorService: UserNotTakenValidatorService
+            private userNotTakenValidatorService: UserNotTakenValidatorService,
+            private signupService: SignUpService,
+            private router: Router
     ){}
     
     ngOnInit(): void {
@@ -48,5 +54,15 @@ export class SignUpComponent implements OnInit {
                 ]
             ]
         })
+    }
+
+    signup(){
+        const newUser = this.signupForm.getRawValue() as NewUser;
+
+        this.signupService.signup(newUser)
+            .subscribe(
+                () => this.router.navigate(['']),
+                err => console.log(err)
+            );
     }
  }
