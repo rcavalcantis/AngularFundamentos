3dd82c5c57c3258dc590dd8a86a6993d2a1ed1ca
Rodrigo Cavalcanti <rodrigo.cavalcanti-silva@gmail.com>
2020-06-03 17:56:50 -0300

    Preview Imagens - fail

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -12,6 +12,7 @@ export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
   file: File;
+  preview: string;
   constructor(
       private formBuilder: FormBuilder,
       private photoService: PhotoService,
@@ -23,17 +24,22 @@ export class PhotoFormComponent implements OnInit {
       file: ['', Validators.required],
       description: ['', Validators.maxLength(300)],
       allowComments: [true]
-    })
+    });
   }
 
-  upload(){
-    //const dados = this.photoForm.getRawValue();
+  upload() {
     const description = this.photoForm.get('description').value;
     const allowComments = this.photoForm.get('allowComments').value;
 
     this.photoService.upload(description, allowComments, this.file)
-      .subscribe( () => { this.router.navigate(['']) } );
-    
+      .subscribe( () => {this.router.navigate([''])} );
+  }
+
+  handleFile(file: File) {
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => this.preview = event.target.result;
+    reader.readAsDataURL(file);
   }
 
 }
