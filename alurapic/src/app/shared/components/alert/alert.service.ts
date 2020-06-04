4e265af76ed60faee9032370e68d7f766f1bae1f
Rodrigo Cavalcanti <rodrigo.cavalcanti-silva@gmail.com>
2020-06-04 14:11:49 -0300

    Build new component of message in remove

---
+++ alurapic/src/app/shared/components/alert/alert.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Alert, AlertType } from './alert';
+
+@Injectable({providedIn: 'root'})
+export class AlertService{
+
+    alertSubject: Subject<Alert> = new Subject<Alert>();
+
+    private alert(alertType: AlertType, message: string){
+        this.alertSubject.next(new Alert(alertType, message));
+    }
+
+    success(message: string){
+        return this.alert(AlertType.SUCCESS, message);
+    }
+
+    warning(message: string){
+        return this.alert(AlertType.WARNING, message);
+    }
+
+    danger(message: string){
+        return this.alert(AlertType.DANGER, message);
+    }
+
+    info(message: string){
+        return this.alert(AlertType.INFO, message);
+    }
+    getAlert(){
+        return this.alertSubject.asObservable();
+    }
+}
